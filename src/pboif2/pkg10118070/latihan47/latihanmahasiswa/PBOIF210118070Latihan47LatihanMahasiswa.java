e7fe3cc1c43e4b5a063999d11a249c30bfa8a8c0
Aufa Ilyasha <55798997+Ailyasha@users.noreply.github.com>
2019-11-07 17:47:25 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118070/latihan47/latihanmahasiswa/PBOIF210118070Latihan47LatihanMahasiswa.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118070.latihan47.latihanmahasiswa;
+
+/**
+ *
+ * @author Ailyasha
+ */
+public class PBOIF210118070Latihan47LatihanMahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NilaiAkhir na1,na2,na3;
+        
+        na1 = new NilaiAkhir(75, 45, 34);
+        na1.tampilNilai();
+        na2 = new NilaiAkhir(60, 80, 75);
+        na2.tampilNilai();
+        na3 = new NilaiAkhir(30, 80, 40);
+        na3.tampilNilai();
+    }
+    
+}
